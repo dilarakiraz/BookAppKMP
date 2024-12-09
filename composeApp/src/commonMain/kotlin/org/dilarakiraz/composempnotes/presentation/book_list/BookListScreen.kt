90855da0c944be61ee2f12d5a1f54a82fd48c71a
dilarakiraz <dilara.kiraz@matriksdata.com>
2024-12-09 21:56:26 +0300

    book list screen

--- composeApp/src/commonMain/kotlin/org/dilarakiraz/composempnotes/presentation/book_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/org/dilarakiraz/composempnotes/presentation/book_list/BookListScreen.kt
@@ -1,11 +1,24 @@
 package org.dilarakiraz.composempnotes.presentation.book_list
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.dilarakiraz.composempnotes.book.domain.Book
+import org.dilarakiraz.composempnotes.presentation.DarkBlue
+import org.dilarakiraz.composempnotes.presentation.book_list.components.BookSearchBar
 import org.koin.compose.viewmodel.koinViewModel
+import kotlin.math.max
 
 
 @Composable
@@ -32,5 +45,27 @@ private fun BookListScreen(
     state: BookListState,
     onAction: (BookListAction) -> Unit,
 ) {
-
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(DarkBlue)
+            .statusBarsPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        BookSearchBar(
+            searchQuery = state.searchQuery,
+            onSearchQueryChange = {
+                onAction(BookListAction.OnSearchQueryChange(it))
+            },
+            onImeSearch = {
+                keyboardController?.hide()
+            },
+            modifier =
+                Modifier
+                    .widthIn(max = 400.dp)
+                    .fillMaxWidth()
+                    .padding(16.dp)
+        )
+    }
 }
--- composeApp/src/commonMain/kotlin/org/dilarakiraz/composempnotes/presentation/book_list/components/BookSearchBar.kt
+++ composeApp/src/commonMain/kotlin/org/dilarakiraz/composempnotes/presentation/book_list/components/BookSearchBar.kt
@@ -1,18 +1,33 @@
 package org.dilarakiraz.composempnotes.presentation.book_list.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key.Companion.R
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import bookappkmp.composeapp.generated.resources.Res
+import bookappkmp.composeapp.generated.resources.close_hint
 import bookappkmp.composeapp.generated.resources.search_hint
 import org.dilarakiraz.composempnotes.presentation.DarkBlue
+import org.dilarakiraz.composempnotes.presentation.DesertWhite
 import org.dilarakiraz.composempnotes.presentation.SandYellow
 import org.jetbrains.compose.resources.stringResource
 
@@ -23,27 +38,66 @@ fun BookSearchBar(
     onImeSearch: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    OutlinedTextField(
-        value = searchQuery,
-        onValueChange = onSearchQueryChange,
-        shape = RoundedCornerShape(100),
-        colors = OutlinedTextFieldDefaults.colors(
-            cursorColor = DarkBlue,
-            focusedBorderColor = SandYellow
-        ),
-        placeholder = {
-            Text(
-                text = stringResource(Res.string.search_hint),
-                color = DarkBlue
-            )
-        },
-        leadingIcon = {
-            Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.66f)
-            )
-        },
-        singleLine = true
-    )
+    CompositionLocalProvider(
+        LocalTextSelectionColors provides TextSelectionColors(
+            handleColor = SandYellow,
+            backgroundColor = SandYellow
+        )
+    ) {
+        OutlinedTextField(
+            value = searchQuery,
+            onValueChange = onSearchQueryChange,
+            shape = RoundedCornerShape(100),
+            colors = OutlinedTextFieldDefaults.colors(
+                cursorColor = DarkBlue,
+                focusedBorderColor = SandYellow
+            ),
+            placeholder = {
+                Text(
+                    text = stringResource(Res.string.search_hint),
+                    color = DarkBlue
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.66f)
+                )
+            },
+            singleLine = true,
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onImeSearch()
+                }
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search
+            ),
+            trailingIcon = {
+                AnimatedVisibility(
+                    visible = searchQuery.isNotBlank()
+                ) {
+                    IconButton(
+                        onClick = {
+                            onSearchQueryChange("")
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = stringResource(Res.string.close_hint),
+                            tint = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
+                }
+            },
+            modifier = modifier
+                .background(
+                    shape = RoundedCornerShape(100),
+                    color = DesertWhite
+                )
+                .minimumInteractiveComponentSize()
+        )
+    }
 }
